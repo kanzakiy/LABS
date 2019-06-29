d6c13b469b3d7ac562691cac2f46bb385648274e
kanzakiy <kanzakiy@ucr.edu>
2019-06-29 01:08:52 -0700

    rm files in working directory

--- runelabs.sh
+++ runelabs.sh
@@ -4,6 +4,7 @@ workdir="C:/Users/YK/Desktop/biot-res/"
 read -p "Type the name of simulation, followed by [enter]:" workname 
 today=$(date "+%Y%m%d")
 mkdir -p "$workdir$workname-$today"
+rm -rf "$workdir$workname-$today/*"
 make 
 cp labs eParameters_IN.txt Parameters_IN.txt SedENV.IN Organisms.IN "$workdir$workname-$today"
 $workdir$workname-$today/labs  "$workdir$workname-$today"
